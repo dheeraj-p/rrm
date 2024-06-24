7da7126d1d38e71f57cc22d9d30d62d2ae26125b
Dheeraj Pundir <dheeraj-p@users.noreply.github.com>
2024-06-24 11:41:49 +0530

    Add APIs to fetch, update and delete a specific room rate

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from api import views
 
 urlpatterns = [
     path("room_rates", views.RoomRateList.as_view()),
+    path("room_rates/<int:room_id>", views.RoomRateDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,3 +19,33 @@ class RoomRateList(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+
+class RoomRateDetail(APIView):
+  def get_object(self, room_id):
+    try:
+      return RoomRate.objects.get(pk=room_id)
+    except RoomRate.DoesNotExist:
+      raise Http404(f"Can't find room rate with room id: {room_id}")
+  
+  def get(self, request, room_id):
+    room_rate = self.get_object(room_id)
+    serializer = RoomRateSerializer(room_rate)
+    return Response(serializer.data)
+
+  def patch(self, request, room_id):
+    room_rate = self.get_object(room_id)
+    if 'room_id' in request.data:
+      return Response({'detail': "Updating room_id is not allowed"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    serializer = RoomRateSerializer(room_rate, data=request.data, partial=True)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  def delete(self, request, room_id):
+    room_rate = self.get_object(room_id)
+    room_rate.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
